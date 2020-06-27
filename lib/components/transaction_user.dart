4163ddd7f9429e991dff99f4d790c7b8f5623dae
Bruno Marthe <brunomarthe@hotmail.com>
2020-06-27 04:00:25 -0300

    adicionando entrada de novos dados e otimizando a lista com ListView.builder

--- lib/components/transaction_user.dart
+++ lib/components/transaction_user.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'transaction_form.dart';
 import 'transaction_list.dart';
@@ -18,19 +20,50 @@ class _TransactionUserState extends State<TransactionUser>{
       date: DateTime.now(),
     ),
     Transaction(
-      id: 't2',
-      title: 'Conta de luz',
+      id: 't20',
+      title: 'Conta de luz #01',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't3',
+      title: 'Conta de luz #02',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't4',
+      title: 'Conta de luz #03',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't5',
+      title: 'Conta de luz #04',
       value: 211.30,
       date: DateTime.now(),
     ),
   ];
 
+  _addTransaction(String title, double value){
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+    
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
+        TransactionForm(_addTransaction),
         TransactionList(_transactions),
-        TransactionForm()
       ],
     );
   }
