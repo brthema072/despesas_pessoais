6cb5992c64c8d3876c32e324e7ace7988e3899be
Bruno Marthe <brunomarthe@hotmail.com>
2020-06-28 05:32:01 -0300

    atualização de layout

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -36,19 +36,32 @@ class Chart extends StatelessWidget {
     });
   }
 
+  double get _weekTotalValue{
+    return groupedTransactions.fold(0.0, (previousValue, element) {
+      return previousValue + element['value'];
+    });
+  } 
+
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
-      margin: EdgeInsets.all(20),
-      child: Row(
-        children: groupedTransactions.map((tr){
-          return ChartBar(
-            label: tr['day'], 
-            value: tr['value'],
-            percentage: 0,
-          );
-        }).toList(),
+      margin: EdgeInsets.all(10),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactions.map((tr){
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                label: tr['day'], 
+                value: tr['value'],
+                percentage: (tr['value'] as double) / _weekTotalValue,
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
